d9d62b038289481f4798e1278c0ea2526d7642fa
Rowoon Lee <dev.rowoon@gmail.com>
2023-03-13 20:37:06 +0900

    [docs] 백로고 이미지 추가

--- src/app/App.js
+++ src/app/App.js
@@ -1,7 +1,9 @@
+import Recruitment from "pages/Recruitment";
+
 function App() {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Recruitment />
     </div>
   );
 }
---
+++ src/components/BackButton.jsx
@@ -0,0 +1,8 @@
+import goBack from '../assets/go-back.svg'
+function BackButton(){
+  return (
+    <img src={goBack} alt='뒤로가기 이미지' />
+  ) 
+}
+
+export default BackButton
