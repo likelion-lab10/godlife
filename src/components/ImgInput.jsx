fe04371fea18b48cd2bd3f49f70fd8e664f12bbf
Rowoon Lee <dev.rowoon@gmail.com>
2023-03-14 21:07:48 +0900

    [docs] form 추가

--- src/components/ImgInput.jsx
+++ src/components/ImgInput.jsx
@@ -1,4 +1,5 @@
 import { useRef } from 'react';
+import SubmitButton from './SubmitButton';
 
 function ImgInput(){
   const imageInput = useRef();
@@ -10,11 +11,12 @@ function ImgInput(){
   return (
     <>
       <input type="file" style={{ display: "none" }} ref={imageInput} />
-      <form>
+      <form name='recruitment'>
         <div className='text-[15px] text-[#8A8A8A] mt-[26px]'>사진등록</div>
         <button className='bg-[#EAEAEA] w-[137px] h-[124px] rounded-[15px] shadow-[0_4px_4px_0_rgba(0,0,0,0.25)] mt-[10px] cursor-pointer' onClick={onCickImageUpload}></button>
         <div className='mt-[47px] mb-[10px] text-[#8A8A8A]'>내용</div>
         <textarea className='bg-[#EAEAEA] w-[329px] h-[208px] rounded-[15px] shadow-[0_4px_4px_0_rgba(0,0,0,0.25)] cursor-text placeholder:indent-2.5 leading-10' placeholder='내용을 입력해 주세요'></textarea>
+        <SubmitButton />
       </form>
     </>
   )
