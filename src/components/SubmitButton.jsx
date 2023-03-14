f070868bdb3ef8fcfbb8832a3ea23e65f69dc6a1
Rowoon Lee <dev.rowoon@gmail.com>
2023-03-14 20:52:14 +0900

    [docs] 폰트 굵기 수정

--- src/components/SubmitButton.jsx
+++ src/components/SubmitButton.jsx
@@ -1,6 +1,6 @@
 function SubmitButton(){
   return(
-    <input className='block text-center bg-[#EAEAEA] w-[334px] h-[52px] mt-[63px] rounded-[100px]' type='submit' placeholder='완료' />
+    <input className='block text-center bg-[#EAEAEA] w-[334px] h-[52px] mt-[63px] rounded-[100px] cursor-pointer' type='submit' placeholder='완료' />
   )
 }
 
