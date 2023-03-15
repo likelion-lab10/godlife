abf874a03dccfd35acc33aeb9ac9b9ed27f2e7b6
Majesty-jun <hkwns01@gmail.com>
2023-03-15 23:02:41 +0900

    [feat] FormInput component type 별 스타일링 추가 #26

--- src/components/FormInput/FormInput.jsx
+++ src/components/FormInput/FormInput.jsx
@@ -1,13 +1,23 @@
 import { useId } from 'react'
 
-export function FormInput({ label, srOnly = false, ...restProps }) {
+export function FormInput({ label, srOnly = true, type = 'text', ...restProps }) {
 
   const id = useId();
+  let style = '';
+
+  switch (type) {
+    case "checkbox":
+      style = 'order-first mr-2';
+      break;
+    default:
+      style = 'border-b border-black w-80 p-1 outline-none';
+      break;
+  }
 
   return (
     <>
       <label className={srOnly ? 'sr-only' : ''} id={id}>{label}</label>
-      <input className={`border-b border-black w-80 p-1 outline-none`} id={id} {...restProps} />
+      <input className={style} id={id} type={type} {...restProps} />
     </>
   )
 }
