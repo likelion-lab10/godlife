37bcb02afaaac2cbcfac68d7089045124a1fe829
Rowoon Lee <dev.rowoon@gmail.com>
2023-03-15 21:02:47 +0900

    [refactor] 사진 추가 코드 수정 [feat] 제출 버튼 누를시 사진 사라지도록

--- src/components/ImgInput.jsx
+++ src/components/ImgInput.jsx
@@ -1,13 +1,11 @@
 import { useRef, useState } from 'react';
-import SubmitButton from './SubmitButton';
-import defaultImage from '../assets/plus.svg'
 import { addDoc, collection } from 'firebase/firestore';
 import { dbService } from 'fbase';
 
 function ImgInput(){
-  const [imgFile, setImgFile] = useState("");
   const [challenge, setChallenge] = useState('');
-  const imgRef = useRef();
+  const [attatchment, setAttatchment] = useState("");
+  const fileInput = useRef();
   const onSubmit = async (e) => {
     e.preventDefault();
     await addDoc(collection(dbService, "challenges"),{
@@ -23,31 +21,37 @@ function ImgInput(){
     setChallenge(value)
   };
   
-  const saveImgFile = () => {
-    const file = imgRef.current.files[0];
+  const onFileChange = (e) => {
+    const {
+      target : { files },
+    } = e;
+    const theFile = files[0]
     const reader = new FileReader();
-      reader.readAsDataURL(file);
-      reader.onloadend = () => {
-          setImgFile(reader.result);
-      };
+    reader.onloadend = (finishedEvent) => {
+      const{
+        currentTarget: {result}
+      } = finishedEvent
+      setAttatchment(result);
+    };
+    reader.readAsDataURL(theFile);
   };
 
-  const onErrorImg = (e) => {
-    e.target.src = defaultImage
-  }
-  
+  const onClearAttachment = () => {
+    setAttatchment(null);
+    fileInput.current.value = null;
+  };
 
   return (
     <>
       <form name='recruitment' onSubmit={onSubmit}>
         <div className='text-h3 text-gray mt-[26px]'>사진등록</div>
         <label htmlFor="profileImg" className='block bg-[#EAEAEA] w-[137px] h-[124px] rounded-[15px] shadow-[0_4px_4px_0_rgba(0,0,0,0.25)] mt-[10px] cursor-pointer'>
-        <img className='-indent-[9999px] block m-auto pt-[20px] w-[120px] h-[110px]' src={imgFile ? imgFile :`../assets/plus.svg`} onError={onErrorImg} alt="이미지"/>
+        {attatchment && <img className='-indent-[9999px] block m-auto pt-[20px] w-[120px] h-[110px]' src={attatchment} alt="이미지"/>}
         </label>
-        <input type="file" style={{ display: "none" }} accept="image/*" id="profileImg" onChange={saveImgFile} ref={imgRef} />
+        <input type="file" style={{ display: "none" }} accept="image/*" id="profileImg" onChange={onFileChange} ref={fileInput} />
         <div className='mt-[47px] mb-[10px] text-gray'>내용</div>
-        <textarea className='bg-[#EAEAEA] w-[329px] h-[208px] rounded-[15px] shadow-[0_4px_4px_0_rgba(0,0,0,0.25)] cursor-text placeholder:indent-2.5 leading-10' value={challenge} onChange={onChange} placeholder='내용을 입력해 주세요'></textarea>
-        <SubmitButton />
+        <textarea className='bg-[#EAEAEA] w-[329px] h-[208px] rounded-[15px] shadow-[0_4px_4px_0_rgba(0,0,0,0.25)] cursor-text placeholder:indent-2.5 leading-10' value={challenge} onChange={onChange} placeholder='내용을 입력해 주세요' ref={fileInput}></textarea>
+        <input className='block text-center bg-[#EAEAEA] w-[334px] h-[52px] mt-[63px] rounded-[100px] cursor-pointer placeholder:text-bigButton text-gray' type='submit' placeholder='완료' onClick={onClearAttachment} />
       </form>
     </>
   )
