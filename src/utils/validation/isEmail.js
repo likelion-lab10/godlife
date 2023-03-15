d1a5157de8aceb03bf5460c79ff0c792153b580b
Majesty-jun <hkwns01@gmail.com>
2023-03-15 14:21:53 +0900

    [fix] Email 유효성 검사 정규식 수정 #26

--- src/utils/validation/isEmail.js
+++ src/utils/validation/isEmail.js
@@ -1,4 +1,4 @@
 export const isEmail = (value) => {
-  const regExp = /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i;
+  const regExp = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i;
   return regExp.test(value);
 };
