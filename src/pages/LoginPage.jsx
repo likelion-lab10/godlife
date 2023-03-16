d104ca779ad5271614c90f683b144b7483ea505d
Majesty-jun <hkwns01@gmail.com>
2023-03-16 15:41:53 +0900

    [feat] Social login MarkUp #29

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -14,6 +14,20 @@ export default function LoginPage() {
         <SubmitButton disabled={false} className="mt-6">로그인</SubmitButton>
       </form>
       <Link className="absolute mt-4 text-gray border-b left-1/2 -translate-x-1/2" to='/register'>회원가입</Link>
+      <div className="flex flex-col justify-center items-center gap-6 mt-40">
+        <SubmitButton className="flex justify-center items-center" social={true}>
+          <img className="mr-2 w-5 h-5" src="assets/kakao.png" alt="카카오톡 아이콘" />
+          카카오로 시작하기
+        </SubmitButton>
+        <SubmitButton className="flex justify-center items-center" social={true}>
+          <img className="mr-2 w-5 h-5" src="assets/google.png" alt="구글 아이콘" />
+          구글로 시작하기
+        </SubmitButton>
+        <SubmitButton className="flex justify-center items-center" social={true}>
+          <img className="mr-2 w-5 h-5" src="assets/facebook.png" alt="페이스북 아이콘" />
+          페이스북으로 시작하기
+        </SubmitButton>
+      </div>
     </>
   )
 }
