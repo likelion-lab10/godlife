c404192e62a7fe2d91c8e8677ca04a6965a860e0
Majesty-jun <hkwns01@gmail.com>
2023-03-19 21:14:56 +0900

    [fix] BackButton, CreateChallengeButton svg 파일 변경 및 위치 지정

--- src/components/CreateChallengeButton/CreateChallengeButton.jsx
+++ src/components/CreateChallengeButton/CreateChallengeButton.jsx
@@ -4,7 +4,7 @@ import { ReactComponent as Plus } from 'assets/plus.svg';
 export default function CreateChallengeButton() {
   const navigate = useNavigate();
   return (
-    <button className='p-1 w-8 h-8 rounded-full' onClick={() => navigate('/recruit')}>
+    <button className='absolute right-7 top-20' onClick={() => navigate('/recruit')}>
       <Plus />
     </button>
   )
