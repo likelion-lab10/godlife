4a680062e253045e1b42bc21ce2994875eed67bd
sm <sungminnim97@gmail.com>
2023-03-19 23:24:35 +0900

    [style] 코드 포맷팅

--- src/components/ChallengeList.jsx
+++ src/components/ChallengeList.jsx
@@ -4,9 +4,9 @@ import exampleData from '../data/exampleData';
 
 const ChallengeList = ({ challenges }) => {
   return (
-    <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-y-1 gap-x-3 mb-8">
+    <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-x-4 mb-8">
       {challenges.map((challenge) => (
-        <div className="w-80 md:w-1/3 lg:w-1/4 p-2" key={challenge.id}>
+        <div className="w-80 p-2" key={challenge.id}>
           <ChallengeCard challenge={challenge} />
         </div>
       ))}
