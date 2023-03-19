71991697f39af3c6128ca87a474d5ee135704191
Majesty-jun <hkwns01@gmail.com>
2023-03-19 21:14:56 +0900

    [fix] BackButton, CreateChallengeButton svg 파일 변경 및 위치 지정

--- src/components/BackButton/BackButton.jsx
+++ src/components/BackButton/BackButton.jsx
@@ -4,7 +4,7 @@ import { ReactComponent as BackArrow } from 'assets/backArrow.svg';
 export default function BackButton() {
   const navigate = useNavigate();
   return (
-    <button className='p-1 w-8 h-8 rounded-full' onClick={() => navigate(-1)}>
+    <button className='absolute left-7 top-20' onClick={() => navigate(-1)}>
       <BackArrow />
     </button>
   )
