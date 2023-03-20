3a3ca48cc4b9e8ee27e3ecfa89169362130dc73d
sm <sungminnim97@gmail.com>
2023-03-20 16:18:00 +0900

    [refactor] 코드 리팩토링

--- src/hooks/useCategoryData.js
+++ src/hooks/useCategoryData.js
@@ -1,3 +1,4 @@
+// 카테고리 배열 반환 훅
 import { useState, useEffect } from 'react';
 import firebase from 'firebase/app';
 import 'firebase/firestore';
@@ -8,19 +9,21 @@ export default function useCategoryData() {
   const [categories, setCategories] = useState([]);
 
   useEffect(() => {
-    // 파이어베이스에서 카테고리 정보 가져오기
-    db.collection('challenges')
-      .get()
-      .then((querySnapshot) => {
-        const categories = [];
-        querySnapshot.forEach((doc) => {
-          const data = doc.data();
-          if (!categories.includes(data.category)) {
-            categories.push(data.category);
-          }
-        });
+    // Firestore에서 challenges 컬렉션을 가져오기
+    const getCategoryData = async () => {
+      try {
+        const querySnapshot = await db.collection('challenges').get();
+        // 카테고리 배열을 생성
+        const categories = querySnapshot.docs
+          .map((doc) => doc.data().category)
+          .filter((category, index, self) => self.indexOf(category) === index);
+        // 카테고리 배열을 상태로 설정
         setCategories(categories);
-      });
+      } catch (error) {
+        console.log('Error getting documents: ', error);
+      }
+    };
+    getCategoryData();
   }, []);
 
   return categories;
