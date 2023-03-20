63899df0aac87dd369e63885960d52edab43912e
Majesty-jun <hkwns01@gmail.com>
2023-03-20 13:25:37 +0900

    [feat] 페이지 라우팅 설정 완료

--- src/app/App.js
+++ src/app/App.js
@@ -1,18 +1,18 @@
-import { BrowserRouter, Routes } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* <Route path="/main" element={ } />
+        <Route path="/" element={ } />
         <Route path="/login" element={ } />
+        <Route path="/recruit" element={ } />
         <Route path="/register" element={ } />
         <Route path="/dashboard" element={ } />
         <Route path="/filter" element={ } />
         <Route path="/search" element={ } />
         <Route path="/certificate" element={ } />
-        <Route path="/recruit" element={ } />
-        <Route path="/participate" element={ } /> */}
+        <Route path="/participate" element={ } />
       </Routes>
     </BrowserRouter>
   );
