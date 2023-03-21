82a069c049bd228c2d975c833cd8013761652e7d
Rowoon Lee <dev.rowoon@gmail.com>
2023-03-21 09:48:36 +0900

    [refector] 필요없는 컴포넌트 삭제

--- src/components/BackButtonSub.jsx
+++
@@ -1,9 +0,0 @@
-import goBack from '../assets/go-back.svg'
-
-function BackButton(){
-  return( 
-    <img className='absolute top-[80px]' src={goBack} alt='뒤로가기 이미지' />
-  )
-}
-
-export default BackButton
