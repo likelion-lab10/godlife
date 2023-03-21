74391d257bc92700de13c75908be08c2a1bf2cdc
sm <sungminnim97@gmail.com>
2023-03-21 14:15:29 +0900

    [feat] 페이지 라우팅

--- src/app/App.js
+++ src/app/App.js
@@ -1,11 +1,33 @@
-import React from 'react';
-import Category from 'pages/category';
+import { BrowserRouter, Routes, Route, useParams } from "react-router-dom";
+import Category from '../components/Category';
+import ChallengeList from '../components/ChallengeList';
+import exampleData from '../data/exampleData';
+
+function Challenge() {
+  const { id } = useParams();
+  
+  return <div>Challenge {id}</div>;
+}
 
 function App() {
   return (
-    <div>
-      <Category />
-    </div>
+    <BrowserRouter basename="/challenge">
+      <div>
+        <Category />
+        <Routes>
+          <Route path="/" element={<ChallengeList challenges={exampleData} />} />
+          <Route path=":id" element={<Challenge />} />
+          {/* <Route path="/login" element={ } />
+          <Route path="/recruit" element={ } />
+          <Route path="/register" element={ } />
+          <Route path="/dashboard" element={ } />
+          <Route path="/filter" element={ } />
+          <Route path="/search" element={ } />
+          <Route path="/certificate" element={ } />
+          <Route path="/participate" element={ } /> */}
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/components/ChallengeCard.jsx
+++ src/components/ChallengeCard.jsx
@@ -1,8 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Link } from 'react-router-dom';
-import { runningImage, readingImage, wakeupImage, drinkingImage, 
-         ploggingImage, zerowasteImage, beachcombingImage, readingnewsImage,
-         meditationImage, saladImage, eatingImage, writingImage } from '../index.js';
+import { Link } from 'react-router-dom';
 
 // 마우스 오버 이벤트
 const ChallengeCard = ({ challenge }) => {
@@ -20,27 +17,25 @@ const ChallengeCard = ({ challenge }) => {
 
   return (
     <div className="w-1/2 mb-4">
-      <BrowserRouter>
-        <Link to={`/challenge/${challenge.id}`}>
-          <div
-            className={`bg-background ${boxShadow} rounded-2xl overflow-hidden`}
-            onMouseEnter={handleMouseEnter}
-            onMouseLeave={handleMouseLeave}
-            >
-            <div className="h-40 overflow-hidden">
-              <img
-                className="h-40 w-full object-cover rounded-t-2xl"
-                src={challenge.imageUrl}
-                alt={challenge.title}
-                style={{ objectFit: 'cover' }}
-              />
-            </div>
-            <div className="px-4 py-2">
-              <h2 className="font-medium text-sm text-center">{challenge.title}</h2>
-            </div>
+      <Link to={`/challenge/${challenge.id}`}>
+        <div
+          className={`bg-background ${boxShadow} rounded-2xl overflow-hidden`}
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
+        >
+          <div className="h-40 overflow-hidden">
+            <img
+              className="h-40 w-full object-cover rounded-t-2xl"
+              src={challenge.imageUrl}
+              alt={challenge.title}
+              style={{ objectFit: 'cover' }}
+            />
           </div>
-        </Link>
-      </BrowserRouter>
+          <div className="px-4 py-2">
+            <h2 className="font-medium text-sm text-center">{challenge.title}</h2>
+          </div>
+        </div>
+      </Link>
     </div>
   );
 };
