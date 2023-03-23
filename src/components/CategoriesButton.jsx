8eb621f280192bcbc96d472e9c73a39ebebad377
Rowoon Lee <dev.rowoon@gmail.com>
2023-03-23 21:55:12 +0900

    [docs] 카테고리 버튼 컴포넌트 추가

---
+++ src/components/CategoriesButton.jsx
@@ -0,0 +1,55 @@
+import calendarIcon from '../assets/calendar.svg';
+import foodIcon from '../assets/food.svg';
+import habitIcon from '../assets/habbit.svg';
+import environmentIcon from '../assets/environment.svg';
+
+const CategoriesButton = ({ filter, selected, handleFilterClick }) => {
+  const iconPaths = {
+    생활습관: calendarIcon,
+    식습관: foodIcon,   
+    취미: habitIcon,
+    환경: environmentIcon,
+  };
+
+  return (
+    <button
+      className={`px-1 py-2 text-xs font-medium leading-4 rounded-full ${
+        selected ? "text-white bg-primary" : "text-gray-700 bg-white"
+      }`}
+      onClick={() => {
+        handleFilterClick(filter);
+        console.log(filter);
+      }}
+    >
+      <div className="flex items-center">
+        {iconPaths[filter] && (
+          <img src={iconPaths[filter]} alt="icon" className="w-4 h-4 mr-2" />
+        )}
+        <div>{filter}</div>
+      </div>
+    </button>
+  );
+};
+
+const CategoryButton = () => {
+  const filters = ["생활습관", "식습관", "취미", "환경"];
+
+  const handleFilterClick = (filter) => {
+    console.log(`클릭된 filter는`);
+  };
+
+  return (
+    <div className="flex gap-1">
+      {filters.map((filter) => (
+        <CategoriesButton
+          key={filter}
+          filter={filter}
+          selected={false}
+          handleFilterClick={handleFilterClick}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default CategoryButton;
--- src/components/Upload.jsx
+++ src/components/Upload.jsx
@@ -3,11 +3,13 @@ import Introduce from './Introduce';
 import Container from './Container';
 import BackButton from './BackButton/BackButton';
 import PageTitle from './PageTitle/PageTitle';
+import CategoryButton from './CategoriesButton';
 function Upload(){
   return (
     <Container>
       <BackButton />
       <PageTitle>모집</PageTitle>
+      <CategoryButton />
       <Introduce />
       <ImgInput />
     </Container>
