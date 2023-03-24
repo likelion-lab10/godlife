c8ac027b1145ba6c80e7a0d94f3829d32327d4d5
simyuseon <yseon0508@naver.com>
2023-03-24 09:08:42 +0900

    feat : 대시보드 페이지 생성

---
+++ src/pages/DashBoardPage.jsx
@@ -0,0 +1,10 @@
+import ChallengDetail from "../components/challengeDetails/ChallengeDetail";
+
+const DashBoardPage = () => {
+  return (
+    <>
+      <ChallengDetail />
+    </>
+  );
+};
+export default DashBoardPage;
