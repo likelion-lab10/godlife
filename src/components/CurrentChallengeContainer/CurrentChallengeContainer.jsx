97cbe4487c1837d68d85f7b3f787a383a757d243
hyunzsu <kkm5169@naver.com>
2023-03-24 15:20:38 +0900

    [docs] 파이어베이스 설치

--- src/components/CurrentChallengeContainer/CurrentChallengeContainer.jsx
+++ src/components/CurrentChallengeContainer/CurrentChallengeContainer.jsx
@@ -1,4 +1,22 @@
 import { CurrentChallengeCard } from 'components';
+import { doc, getDoc, onSnapshot } from 'firebase/firestore';
+import { db } from 'fbase';
+
+
+const docRef = doc(db, 'challenges2', '1')
+
+const docSnap = await getDoc(docRef);
+try {
+  if (docSnap.exists()) {
+    console.log("Document data:", docSnap.data());
+  } else {
+    console.log("No such document!");
+  }
+} catch (error) {
+  console.log("Error getting document:", error);
+}
+
+
 
 function CurrentChallengeContainer() {
   return (
