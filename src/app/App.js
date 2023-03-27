e1f54f8c734a5bae273f525fa29d7ae923edc71c
Majesty-jun <hkwns01@gmail.com>
2023-03-27 15:55:03 +0900

    [feat] KakaoLogin test용 코드 #29

--- src/app/App.js
+++ src/app/App.js
@@ -1,18 +1,19 @@
+import KakaoLogin from "components/KakaoLogin/KakaoLogin";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={ } />
-        <Route path="/login" element={ } />
+        <Route path="/" element={<KakaoLogin /> } />
+        {/* <Route path="/login" element={ } />
         <Route path="/recruit" element={ } />
         <Route path="/register" element={ } />
         <Route path="/dashboard" element={ } />
         <Route path="/filter" element={ } />
         <Route path="/search" element={ } />
         <Route path="/certificate" element={ } />
-        <Route path="/participate" element={ } />
+        <Route path="/participate" element={ } /> */}
       </Routes>
     </BrowserRouter>
   );
