2123f6bc8ac70922b90116ed3aa0f71ac3c47b1d
Majesty-jun <hkwns01@gmail.com>
2023-03-27 11:23:24 +0900

    [feat] 구글, 페이스북 로그인 컴포넌트 index.js에 추가 #29

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,7 @@ export { default as Navbar } from './Navbar/Navbar';
 export { default as TextInput } from './TextInput/TextInput';
 export { default as PageTitle } from './PageTitle/PageTitle';
 export { default as BackButton } from './BackButton/BackButton';
+export { default as GoogleLogin } from './GoogleLogin/GoogleLogin';
 export { default as SubmitButton } from './SubmitButton/SubmitButton';
+export { default as FacebookLogin } from './FacebookLogin/FacebookLogin';
 export { default as CreateChallengeButton } from './CreateChallengeButton/CreateChallengeButton';
