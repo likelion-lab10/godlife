f673a18f84d9852b36badf6b0708496b3171d311
Rowoon Lee <dev.rowoon@gmail.com>
2023-03-27 17:18:14 +0900

    [docs] 코드 제거

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -19,32 +19,6 @@ function Input(){
   const onSubmit = async (e) => {
     e.preventDefault();
     let attachmentUrl = '';
-    const displayCreateAt = (createdAt) => {
-      const date = new Date(createdAt);
-      const now = Date.now();
-      const milliSeconds = now - date;
-  
-      const seconds = milliSeconds / 1000;
-      const minutes = seconds / 60;
-      const hours = minutes / 60;
-      const days = hours / 24;
-      const months = days / 30;
-      const years = months / 12;
-  
-      if (seconds < 60) {
-        return "방금 전";
-      } else if (minutes < 60) {
-        return `${Math.floor(minutes)}분 전`;
-      } else if (hours < 24) {
-        return `${Math.floor(hours)}시간 전`;
-      } else if (days < 30) {
-        return `${Math.floor(days)}일 전`;
-      } else if (months < 12) {
-        return `${Math.floor(months)}달 전`;
-      } else {
-        return `${Math.floor(years)}년 전`;
-      }
-    };
     if(attachment !== ""){
       const attachmentRef = ref(storageService, `${uuid4()}`);
       const response = await uploadString(attachmentRef, attachment, "data_url");
@@ -52,7 +26,7 @@ function Input(){
     } 
     const challengObj = {
       challenge,
-      createdAt: displayCreateAt(Date.now()),
+      createdAt,
       attachmentUrl,
       hashTags,
       category : filter
