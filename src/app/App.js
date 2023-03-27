43cf1d0fbb5fbf4a298a1828171089fc7a617444
Rowoon Lee <dev.rowoon@gmail.com>
2023-03-27 11:50:04 +0900

    [docs] forwardRef 해결중

--- src/app/App.js
+++ src/app/App.js
@@ -1,25 +1,23 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginPage from "pages/LoginPage";
 import RegisterPage from "pages/RegisterPage";
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Main from '../pages/Main';
+import Main from "../pages/Main";
 import Recruitment from "pages/Recruit";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Certificate from "../pages/Certificate";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Main />} />
-        <Route path="/login" element={<LoginPage /> } />
+        <Route path="/" element={<Main />} />
+        <Route path="/login" element={<LoginPage />} />
         <Route path="/recruit" element={<Recruitment />} />
-        <Route path="/register" element={<RegisterPage /> } />
-        <Route path="/dashboard" element={ } />
-        <Route path="/filter" element={ } />
-        <Route path="/search" element={ } />
+        <Route path="/register" element={<RegisterPage />} />
+        {/* <Route path="/dashboard" element={ } /> */}
+        {/* <Route path="/filter" element={ } /> */}
+        {/* <Route path="/search" element={ } /> */}
         <Route path="/certificate" element={<Certificate />} />
-        <Route path="/participate" element={ } />
+        {/* <Route path="/participate" element={ } /> */}
       </Routes>
     </BrowserRouter>
   );
--- src/components/Tag.jsx
+++ src/components/Tag.jsx
@@ -1,6 +1,7 @@
 import isEmptyValue from '../utils/isEmptyValue';
+import { forwardRef } from 'react';
 
-function Tag({...restProps}){
+const Tag = forwardRef(({...restProps}, ref) => {
   const { inputHashTag, handleInputHashTag, hashTags, handleHashTags} = restProps;
   const addHashTag = (e) => {
     const allowedCommand = ['Comma', 'Enter', 'Space', 'NumpadEnter'];
@@ -54,7 +55,7 @@ function Tag({...restProps}){
     <div className='bg-[#EAEAEA] w-[334px] h-[57px] rounded-[15px] shadow-[0_4px_4px_0_rgba(0,0,0,0.25)] flex items-center flex-wrap min-h-[50px] px-[10px] focus-within:bg-tomato'>
       {hashTags.map((hashTag, index) => {
           return (
-            <div key={index} className='flex justify-between items-center m-[5px] p-[5px] bg-[tomato] rounded-[5px] text-white text-[13px] min-w-[50px]'>
+            <div ref={ref} key={index} className='flex justify-between items-center m-[5px] p-[5px] bg-[tomato] rounded-[5px] text-white text-[13px] min-w-[50px]'>
               <span className='pt-[3px] pl-[2px]'>{hashTag}</span>
               <button onClick={deleteTagItem} className='flex justify-center items-center w-[15px] h-[15px] ml-[5px] mt-[3px]'>X</button>
             </div>
@@ -63,7 +64,7 @@ function Tag({...restProps}){
         <input type='text' placeholder='태그를 입력해주세요' onChange={changeHashTagInput} value={inputHashTag} onKeyUp={addHashTag} onKeyDown={keyDownHandler} className='inline-flex bg-transparent border-none outline-none cursor-text' />
     </div> 
   )
-      }
+});
 
 
 export default Tag
