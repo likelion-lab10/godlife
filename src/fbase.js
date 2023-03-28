d9be1631f1863068c86c77dd7d95c4f2133d1744
sm <sungminnim97@gmail.com>
2023-03-28 09:44:01 +0900

    [refactor] 코드 리팩토링

--- src/fbase.js
+++ src/fbase.js
@@ -2,31 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getFirestore, collection, getDocs  } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
-// const {
-//   REACT_APP_API_KEY,
-//   REACT_APP_AUTH_DOMAIN,
-//   REACT_APP_PROJECT_ID,
-//   REACT_APP_MESSAGE_SENDER_ID,
-//   REACT_APP_STORAGE_BUCKET,
-//   REACT_APP_APP_ID,
-// } = process.env;
-
-// const firebaseConfig = {
-//   apiKey: REACT_APP_API_KEY,
-//   authDomain: REACT_APP_AUTH_DOMAIN,
-//   projectId: REACT_APP_PROJECT_ID,
-//   storageBucket: REACT_APP_STORAGE_BUCKET,
-//   messagingSenderId: REACT_APP_MESSAGE_SENDER_ID,
-//   appId: REACT_APP_APP_ID,
-// };
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAwdtVK3P5fIZCd-ZvKlYS_H9oQyLPZjlg",
-  authDomain: "test6-59dbb.firebaseapp.com",
-  projectId: "test6-59dbb",
-  storageBucket: "test6-59dbb.appspot.com",
-  messagingSenderId: "138389934575",
-  appId: "1:138389934575:web:8b73fcc9bcb103fc6a661a"
+  apiKey: "AIzaSyDhKr4_yy8tG_C8gSx-LpWl2fKjHzFq30Y",
+  authDomain: "godlife2-63573.firebaseapp.com",
+  projectId: "godlife2-63573",
+  storageBucket: "godlife2-63573.appspot.com",
+  messagingSenderId: "550938668999",
+  appId: "1:550938668999:web:64eb38c227f95912ab73e2"
 };
 
 
