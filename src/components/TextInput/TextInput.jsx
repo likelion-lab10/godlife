f89d26dc24c4f6a9e32471ad43926d8f61883b48
Majesty-jun <hkwns01@gmail.com>
2023-03-28 20:42:02 +0900

    [style] TextInput 컴포넌트 배경색 변경

--- src/components/TextInput/TextInput.jsx
+++ src/components/TextInput/TextInput.jsx
@@ -8,7 +8,7 @@ export function TextInput({ children, type = 'text', name, ...restProps }) {
     <>
       <label className='sr-only' id={id}>{children}</label>
       <input
-        className='border-b border-black w-80 p-1 outline-none'
+        className='border-b border-black w-80 p-1 outline-none bg-[#f5f5f5]'
         id={id}
         type={type}
         name={name}
